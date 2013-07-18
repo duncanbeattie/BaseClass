9650d18b48c3f5e6e1abbe0f517303ba73f824c5
Duncan Beattie <hello@duncanbeattie.com>
2013-07-18 12:15:13 +0100

    First Commit
    Adding readme and main function

---
+++ baseClass.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	/* add path classes to body tag */
+	classpaths($('body'))
+}
+
+/* BaseClass */
+/* take document path and add each level as a class attribute on the object passed in */
+function baseClass(o){
+	if(typeof String.prototype.trim !== 'function') {String.prototype.trim = function(){return this.replace(/^\s+|\s+$/g, '')}}
+	var cls=document.location.toString().split(document.domain)[1].toString().replace(/-|\s/g, "").replace(/\//g,' ').trim();
+	o.attr('class') ? o.attr('class',o.attr('class')+' '+cls) : o.attr('class',cls)
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+**BaseClass**
+simple little function to take the document path and add each level as a class to a specified object.
